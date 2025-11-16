40f9beb5b46e4caf5da77c60d4091dce2fa1a167
AryanMadan3107 <aryanmadan@outlook.com>
2025-11-16 11:52:37 +0100

    Add files via upload

--- FlappyBird.py
+++ FlappyBird.py
@@ -14,8 +14,33 @@ bg=pygame.image.load("Lesson 7/images/bg.png")
 ground=pygame.image.load("Lesson 7/images/ground.png")
 groundx=0
 
-run=True
+class Bird(pygame.sprite.Sprite):
+    def __init__(self, x,y):
+        pygame.sprite.Sprite.__init__(self)
+        self.images = []
+        self.index = 0
+        for i in range(1,4):
+            img = pygame.image.load(f'Lesson 7/images/bird{i}.png')
+            self.images.append(img)
 
+        self.image = self.images[self.index]
+        self.rect = self.image.get_rect()
+        self.rect.center = [x,y]
+        self.counter=0
+    def update(self):
+        self.counter+=1
+        if self.counter > 5:
+            self.index +=1
+            if self.index > 2:
+                self.index=0
+            self.image = self.images[self.index]
+            self.counter=0
+
+birdgroup = pygame.sprite.Group()
+fb = Bird(100,250)
+birdgroup.add(fb)
+
+run=True
 while run:
     clock.tick(Fps) 
     for event in pygame.event.get():
@@ -26,4 +51,6 @@ while run:
     groundx-=2   
     if groundx<-100:
         groundx=0
+    birdgroup.draw(screen)
+    birdgroup.update()
     pygame.display.update()
