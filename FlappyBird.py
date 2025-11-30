9c464ce777962159a78902d0983199632e9bec13
AryanMadan3107 <aryanmadan@outlook.com>
2025-11-30 11:59:02 +0100

    Add files via upload

--- FlappyBird.py
+++ FlappyBird.py
@@ -1,4 +1,5 @@
 import pygame
+import random
 
 
 pygame.init()
@@ -7,6 +8,11 @@ WIDTH=800
 HEIGHT=600
 Fps=60
 clock=pygame.time.Clock()
+pipe_frequency = 1500
+last_pipe = pygame.time.get_ticks() - pipe_frequency
+
+score = 0
+scorefont=pygame.font.SysFont("Arial",30)
 
 screen=pygame.display.set_mode((WIDTH,HEIGHT))
 
@@ -55,10 +61,29 @@ class Bird(pygame.sprite.Sprite):
             if pygame.mouse.get_pressed()[0] == 0:
                     self.clicked=False
             self.image=pygame.transform.rotate(self.images[self.index],self.velocity*-4)
+        if gameover==True:
+            self.image=pygame.transform.rotate(self.images[self.index],self.velocity*0)
 
+class Pipe(pygame.sprite.Sprite):
+    def __init__(self, x,y,position):
+        pygame.sprite.Sprite.__init__(self)     
+        self.image = pygame.image.load("Lesson 7/images/pipe.png")
+        self.rect = self.image.get_rect()
+        if position==1:
+            self.image=pygame.transform.flip(self.image,False,True)
+            self.rect.bottomleft=[x,y]
+        if position==-1:
+            self.rect.topleft=[x,y]
+    def update(self):
+        self.rect.x-=5
+        if self.rect.right<0:
+            self.kill()
 birdgroup = pygame.sprite.Group()
 fb = Bird(100,250)
 birdgroup.add(fb)
+
+pipegroup = pygame.sprite.Group()
+
 
 run=True
 while run:
@@ -69,10 +94,24 @@ while run:
         if event.type == pygame.MOUSEBUTTONDOWN:
             flying=True
     screen.blit(bg,(0,0))
-    screen.blit(ground,(groundx,550))
-    groundx-=2   
-    if groundx<-100:
-        groundx=0
+    scoretxt=scorefont.render(str(score),True,"white")
+    screen.blit(scoretxt,(400,50))
+    pipegroup.draw(screen)
+    if gameover==False:
+        groundx-=2
+        if groundx<-100:
+            groundx=0
     birdgroup.draw(screen)
     birdgroup.update()
+    time_now=pygame.time.get_ticks()
+    if gameover==False and flying==True:
+        if time_now - last_pipe>pipe_frequency:
+            pipegap=random.randint(50,100)
+            tp=Pipe(800,300-pipegap,1)
+            pipegroup.add(tp)
+            bp=Pipe(800,300+pipegap,-1)
+            pipegroup.add(bp)
+            last_pipe=time_now
+    screen.blit(ground,(groundx,550))
+    pipegroup.update()
     pygame.display.update()
